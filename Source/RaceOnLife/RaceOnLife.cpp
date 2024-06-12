81b87901870e7902a8ae7742263f2536a5bf8516
UnknwnDev1 <unknowndev71@gmail.com>
2024-06-12 04:11:19 +0300

    first_commit_little

---
+++ Source/RaceOnLife/RaceOnLife.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RaceOnLife.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RaceOnLife, "RaceOnLife" );
